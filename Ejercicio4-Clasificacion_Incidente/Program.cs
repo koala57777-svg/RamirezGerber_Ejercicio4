bb0fcea788a793695597b3d16cbdf9118703e525
Gerber Ramírez <koala57777@gmail.com>
2026-02-21 20:53:20 -0600

    Se implementó la validación de datos de entrada.

--- Ejercicio4-Clasificacion_Incidente/Program.cs
+++ Ejercicio4-Clasificacion_Incidente/Program.cs
@@ -4,6 +4,7 @@ Console.WriteLine("1. Malware");
 Console.WriteLine("2. Phishing");
 Console.WriteLine("3. Acceso no autorizado");
 Console.WriteLine("4. Fuga de información");
+Console.WriteLine("");
 int tipoIncidente = int.Parse(Console.ReadLine());
 Console.WriteLine("");
 Console.WriteLine("Ingrese el activo afectado (Número): ");
@@ -12,13 +13,31 @@ Console.WriteLine("1. PC");
 Console.WriteLine("2. Servidor");
 Console.WriteLine("3. Personales");
 Console.WriteLine("4. Red");
+Console.WriteLine("");
 int activoAfectado = int.Parse(Console.ReadLine());
 Console.WriteLine("");
 Console.WriteLine("Ingrese el tipo de dato que fue comprometido (Número): ");
+Console.WriteLine("");
+Console.WriteLine("1. Ninguno");
+Console.WriteLine("2. Internos");
+Console.WriteLine("3. Personales");
+Console.WriteLine("4. Financieros");
+Console.WriteLine("");
 int tipoDatoComprometido = int.Parse(Console.ReadLine());
 Console.WriteLine("");
 Console.WriteLine("¿Hay persistencia? (S/N): ");
+Console.WriteLine("");
 string persistencia = Console.ReadLine();
 Console.WriteLine("");
 Console.WriteLine("Ingrese el número de usuarios afectado: ");
+Console.WriteLine("");
 int numeroUsuariosAfectados = int.Parse(Console.ReadLine());
+
+if (tipoDatoComprometido>=1 && tipoDatoComprometido<=4 && (persistencia=="S" || persistencia=="N") && numeroUsuariosAfectados >= 0)
+{
+
+}
+else
+{
+    Console.WriteLine("Datos inváidos. Por favor ingresar datos válidos.");
+}
